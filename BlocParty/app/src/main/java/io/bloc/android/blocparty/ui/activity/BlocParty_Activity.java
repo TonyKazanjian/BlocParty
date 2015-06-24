f00bf0d1e2c8e8ed35dd4026e7ada9add11ecab9
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-06-23 17:44:49 -0700

    added facebook sdk

---
+++ BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/BlocParty_Activity.java
@@ -0,0 +1,7 @@
+package io.bloc.android.blocparty.ui.activity;
+
+/**
+ * Created by tonyk_000 on 6/21/2015.
+ */
+public class BlocParty_Activity {
+}
---
+++ BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/fragment/Login_Fragment.java
@@ -0,0 +1,7 @@
+package io.bloc.android.blocparty.ui.activity.fragment;
+
+/**
+ * Created by tonyk_000 on 6/23/2015.
+ */
+public class Login_Fragment {
+}
