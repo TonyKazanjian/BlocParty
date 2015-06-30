cd0a6865c4f727740a10be33cb9488bafa04bc46
Tony Kazanjian <tony.kazanjian@gmail.com>
2015-06-30 00:50:58 -0700

    instagram login button appears, NetworkOnMainThreadException

--- BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/BlocParty_Activity.java
+++ BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/BlocParty_Activity.java
@@ -1,8 +1,9 @@
 package io.bloc.android.blocparty.ui.activity;
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -14,6 +15,12 @@ import com.facebook.login.LoginResult;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.InstagramAuthService;
+import org.jinstagram.auth.model.Token;
+import org.jinstagram.auth.model.Verifier;
+import org.jinstagram.auth.oauth.InstagramService;
+
 import io.bloc.android.blocparty.R;
 import io.fabric.sdk.android.Fabric;
 
@@ -21,11 +28,14 @@ import io.fabric.sdk.android.Fabric;
  * Created by tonyk_000 on 6/21/2015.
  */
 public class
-        BlocParty_Activity extends Activity {
+        BlocParty_Activity extends FragmentActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "EMBNCj1Iv4NxCOdTHpNZm1rDo";
     private static final String TWITTER_SECRET = "kditozgdp19bQnh3AzNpDf0eJ7ZNfjXU4Drpa37KTBvC5QdqnI";
+    private static final String INSTAGRAM_KEY = "db91bf03212344ff89f2cc6995088cc0";
+    private static final String INSTAGRAM_SECRET = "714c5647104f43149915207c3d188761";
+    private static final Token EMPTY_TOKEN = null;
 
 
     CallbackManager callbackManager = new CallbackManager() {
@@ -39,19 +49,34 @@ public class
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result to the fragment, which will then pass the result to the login
+        // button.
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment);
+        if (fragment != null && !fragment.isVisible() && !fragment.isAdded()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         FacebookSdk.sdkInitialize(this.getApplicationContext());
-        setContentView(R.layout.login_activity);
+        //getting instagram access token
+        InstagramService service = new InstagramAuthService()
+                .apiKey(INSTAGRAM_KEY)
+                .apiSecret(INSTAGRAM_SECRET)
+                .callback("http://localhost:8080/BasicWebDemo/handleInstagramToken/")
+                .build();
+        //getting access token
+        String authorizationUrl = service.getAuthorizationUrl(null);
+        Verifier verifier = new Verifier("verifier you get from the user");
+        Token accessToken = service.getAccessToken(null, verifier);
+        //generate instagram object
+        Instagram instagram = new Instagram(accessToken);
+        setContentView(R.layout.activity_blocparty);
 
         callbackManager = CallbackManager.Factory.create();
-
-
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
--- BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/fragment/Login_Fragment.java
+++ BlocParty/app/src/main/java/io/bloc/android/blocparty/ui/activity/fragment/Login_Fragment.java
@@ -4,11 +4,12 @@ package io.bloc.android.blocparty.ui.activity.fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -16,10 +17,8 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
@@ -33,6 +32,7 @@ public class Login_Fragment extends Fragment {
 
     TwitterLoginButton twitterLoginButton;
     LoginButton loginButton;
+    ImageButton instagramLoginButton;
     private CallbackManager callbackManager = new CallbackManager() {
         @Override
         public boolean onActivityResult(int i, int i1, Intent intent) {
@@ -40,33 +40,31 @@ public class Login_Fragment extends Fragment {
         }
     };
 
-public void onAttach (Activity activity){
-    super.onAttach(activity);
-}
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
     }
 
-    @Nullable
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+  }
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.login_activity, container, false);
+        super.onCreateView(inflater,container,savedInstanceState);
 
         twitterLoginButton = (TwitterLoginButton) view.findViewById(R.id.twitter_login_button);
         twitterLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                TwitterSession session =
-                        Twitter.getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                String token = authToken.token;
-                String secret = authToken.secret;
+            Log.d("Twitter login: ", "working");
             }
 
             @Override
@@ -75,13 +73,12 @@ public void onAttach (Activity activity){
             }
         });
 
-
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
         // If using in a fragment
         loginButton.setFragment(this);
         // Other app specific specialization
-
+        instagramLoginButton = (ImageButton) view.findViewById(R.id.instagram_login_button);
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -99,21 +96,12 @@ public void onAttach (Activity activity){
                 // App code
             }
         });
-        return view; }
+        return view;
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
-        // Pass the activity result to the fragment, which will
-        // then pass the result to the login button.
-//        Fragment fragment = getFragmentManager()
-//                .findFragmentById(R.id.twitter_login_button);
-//        if (fragment != null) {
-//            fragment.onActivityResult(requestCode, resultCode, data);
-//        }
     }
-
-
-    }
+}
